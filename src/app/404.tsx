98bf11f693b4f6cbedaa2207f168195d4ff12990
kirilchristov <kiro.mm@gmail.com>
2024-12-12 16:55:39 +0200

    404 refactor

--- src/app/404.tsx
+++ src/app/404.tsx
@@ -1,25 +1,8 @@
-// app/404.tsx
-'use client';
-
-import {Suspense} from 'react';
-import {useSearchParams} from 'next/navigation';
-
-function NotFoundContent() {
-  const searchParams = useSearchParams();
-  const searchTerm = searchParams?.get('searchTerm');
-
-  return (
-    <div>
-      <h1>Page Not Found</h1>
-      <p>Search Term: {searchTerm || 'Not Found'}</p>
-    </div>
-  );
-}
-
 export default function NotFound() {
   return (
-    <Suspense fallback={<p>Loading...</p>}>
-      <NotFoundContent />
-    </Suspense>
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>The requested page does not exist.</p>
+    </div>
   );
 }
