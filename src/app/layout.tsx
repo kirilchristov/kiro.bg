6e7a2d3eff33962efd9fbd204b1b9ccdc4184fd2
Kiril Christov <kiril.christov@cyberark.com>
2025-07-10 19:00:34 +0300

    fix: chnage from tag manager to analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,15 @@ import {ReactNode} from 'react';
 import {Provider as ChakraThemeProvider} from '@/components/ui/provider';
 import styles from './layout.module.scss';
 import Header from '../components/Header/Header';
-import {GoogleTagManager} from '@next/third-parties/google';
 import {Metadata} from '../components/Metadata/Metadata';
 import {SearchProvider} from '@/components/Search/SearchProvider/SearchProvider';
 import {SpeedInsights} from '@vercel/speed-insights/next';
+import {GoogleAnalytics} from '@next/third-parties/google';
 
 export default function RootLayout({children}: {children: ReactNode}) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <GoogleTagManager gtmId="G-G81R4R8ZKX" />
+      <GoogleAnalytics gaId="G-G81R4R8ZKX" />
       <Metadata />
       <body className={styles.appContainer}>
         <ChakraThemeProvider>
